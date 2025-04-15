332bf194a7287ae64bb4fa527e56aef79b88d9b2
Iman Hosseini Pour <imanhpr1999@gmail.com>
2025-04-15 13:03:52 +0330

    deploy action

--- packages/backend/src/services/root.ts
+++ packages/backend/src/services/root.ts
@@ -1,10 +1,11 @@
 import fp from "fastify-plugin";
+import os from "node:os";
 
 export default fp(
   function rootServicePlugin(fastify, _, done) {
     const version = process.env.npm_package_version;
     const name = "Zarsad";
-    const response = Object.freeze({ name, version });
+    const response = Object.freeze({ name, version, host: os.hostname() });
     fastify.get("/", async function rootHandler() {
       return response;
     });
