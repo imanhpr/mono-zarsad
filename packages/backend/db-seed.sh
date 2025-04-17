2c20646de8c90269c274cc26f7797700aedddfe0
Iman Hosseini Pour <imanhpr1999@gmail.com>
2025-04-17 16:39:29 +0330

    db seeder

---
+++ packages/backend/db-seed.sh
@@ -0,0 +1,4 @@
+npx -w packages/backend mikro-orm-esm seeder:run -c CurrencyTypeSeeder
+npx -w packages/backend mikro-orm-esm seeder:run -c CurrencyPriceSeeder
+npx -w packages/backend mikro-orm-esm seeder:run -c CompanyInfoSeeder
+echo 'success'
