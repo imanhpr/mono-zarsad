b2010f9cceb2571a19000c0817097eb2796960f5
Iman Hosseini Pour <imanhpr1999@gmail.com>
2025-04-18 20:44:30 +0330

    vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      react(),
+      tailwindcss(),
+    ],
+  };
 });
