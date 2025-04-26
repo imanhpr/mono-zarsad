f5e53f10bdbefd5463867f4c23289b6ebce99c7d
Iman Hosseini Pour <imanhpr1999@gmail.com>
2025-04-26 17:27:10 +0330

    sidebar

--- packages/frontend/src/components/SideBar.tsx
+++ packages/frontend/src/components/SideBar.tsx
@@ -1,12 +1,21 @@
-import { Fragment, useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import clsx from "clsx";
 import { ShowSideBarContext } from "../context/contexts";
-import { Link } from "@tanstack/react-router";
+import { Link, useRouter } from "@tanstack/react-router";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function SideBar() {
   const { showSideBar, setShowSideBar } = useContext(ShowSideBarContext);
+  const [currentPath, setCurrentPath] = useState("/");
+  const router = useRouter();
 
+  useEffect(() => {
+    const unsub = router.subscribe("onBeforeLoad", (e) => {
+      setCurrentPath(e.toLocation.href);
+    });
+    return unsub;
+  });
   const f = document.createDocumentFragment();
   useEffect(() => {
     document.body.insertBefore(f, document.body.firstChild);
@@ -18,13 +27,13 @@ export default function SideBar() {
   const Overlay = (
     <div
       onClick={closeHandler}
-      className={`md:hidden absolute inset-0 bg-black opacity-60 fixed ${showSideBar ? "hidden" : "block"} z-10`}
+      className={`md:hidden inset-0 bg-black opacity-60 fixed ${showSideBar ? "hidden" : "block"} z-10`}
     ></div>
   );
-
+  console.log(currentPath);
   const OverlayPortal = createPortal(Overlay, document.body);
   const clsNames = clsx(
-    "bg-slate-800",
+    "bg-zinc-700 border-slate-300 border-l-1",
     "right-0",
     "md:static",
     "fixed",
@@ -37,15 +46,35 @@ export default function SideBar() {
     "md:block",
     "z-20"
   );
+  const normalCls = "flex gap-x-2";
   return (
-    <Fragment>
+    <AnimatePresence>
       <div className={clsNames}>
-        <nav className="flex flex-col m-6 text-white text-2xl" dir="rtl">
-          <Link to="/">خانه</Link>
-          <Link to="/about">درباره ما</Link>
+        <nav
+          className="flex flex-col justify-center gap-y-2 mt-44 mr-6 text-yellow-500 text-xl"
+          dir="rtl"
+        >
+          <div className={normalCls}>
+            {currentPath === "/" ? (
+              <motion.div layoutId="nav-tab" className="border-2"></motion.div>
+            ) : (
+              <div></div>
+            )}
+            <Link className="" to="/">
+              خانه
+            </Link>
+          </div>
+          <div className={normalCls}>
+            {currentPath === "/about" ? (
+              <motion.div layoutId="nav-tab" className="border-2"></motion.div>
+            ) : (
+              <div></div>
+            )}
+            <Link to="/about">درباره ما</Link>
+          </div>
         </nav>
       </div>
       {OverlayPortal}
-    </Fragment>
+    </AnimatePresence>
   );
 }
